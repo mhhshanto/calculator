4cb364df461e3f8fef490d4bbc989500d8034220
shanto <hhassibul@gmail.com>
2022-06-04 23:14:00 +0600

    this is shanto

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 function onee(){
   var disp = document.getElementById("display").innerHTML
   document.getElementById('display').innerText = disp + 1;
-  document.getElementById('ppp').innerText = 1;
+  var dispp1 =document.getElementById('p1').innerText;
+  document.getElementById('p1').innerText = dispp1 + 1;
 }
 function five(){
-  const disp = document.getElementById("display").innerHTML
-  document.getElementById('display').innerHTML = disp + 5
+  var disp = document.getElementById("display").innerHTML
+  document.getElementById('display').innerText = disp + 5;
+  var dispp5 =document.getElementById('p5').innerText;
+  document.getElementById('p5').innerText = dispp5 + 5;
 }
 
 function plus(){
   const disp = document.getElementById("display").innerHTML
-  const disp2 = document.getElementById("ppp").innerHTML
+  const disp2 = document.getElementById("p1").innerHTML
   document.getElementById('display').innerHTML = disp + "+"
-  document.getElementById('ppp').innerHTML = disp2 + "plus"
+  // document.getElementById('p1').innerHTML = disp2 + "plus"
 }
 
 function clearee(){
   document.getElementById("display").innerText = "";
+  document.getElementById('p1').innerHTML= 0
+  document.getElementById('p5').innerHTML = 0
+
 }
 
 function equal(){
+  const number1 = parseFloat(document.getElementById('p1').innerHTML);
+  const number5 = parseFloat(document.getElementById('p5').innerHTML);
+  document.getElementById("display").innerHTML = number1 + number5;
+
+
 
 }
-console.log("hi");
